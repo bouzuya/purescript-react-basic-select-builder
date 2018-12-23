18dcdb3f6f8767296222572ee579cdf96d2341fe
bouzuya <m@bouzuya.net>
2018-12-23 13:26:00 +0900

    add item list

--- src/Component/App.purs
+++ src/Component/App.purs
@@ -2,17 +2,27 @@ module Component.App
   ( app
   ) where
 
-import React.Basic (Component, JSX, Self, StateUpdate(..), createComponent, make)
+import Prelude
+
+import Data.Array as Array
+import Data.Functor (mapFlipped)
+import Data.Maybe (fromMaybe)
+import React.Basic (Component, JSX, Self, StateUpdate(..), capture, createComponent, make)
 import React.Basic.DOM as H
+import React.Basic.DOM.Events (preventDefault, targetValue)
 
 type Props =
   {}
 
 type State =
-  {}
+  { item :: String
+  , items :: Array String
+  }
 
 data Action
   = Noop
+  | AddItem
+  | EditItem String
 
 component :: Component Props
 component = createComponent "App"
@@ -22,10 +32,12 @@ app = make component { initialState, render, update } {}
 
 initialState :: State
 initialState =
-  {}
+  { item: ""
+  , items: []
+  }
 
 render :: Self Props State Action -> JSX
-render self =
+render self@{ state: { item, items } } =
   H.div
   { className: "app"
   , children:
@@ -37,7 +49,43 @@ render self =
         ]
       }
     , H.div
-      { className: "body" }
+      { className: "body"
+      , children:
+        [ H.div_
+          [ H.label_
+            [ H.span_ [ H.text "item" ]
+            , H.input
+              { onChange:
+                  capture
+                    self
+                    targetValue
+                    (\v -> EditItem (fromMaybe "" v))
+              , value: item
+              }
+            ]
+          , H.br {}
+          , H.button
+            { onClick:
+                capture
+                  self
+                  preventDefault
+                  (const AddItem)
+            , children: [ H.text "OK" ]
+            }
+          ]
+        , H.div_
+          [ H.ul_
+            (mapFlipped
+              items
+              (\i ->
+                H.li_
+                [ H.text i
+                ]
+              )
+            )
+          ]
+        ]
+      }
     , H.div
       { className: "footer" }
     ]
@@ -45,3 +93,8 @@ render self =
 
 update :: Self Props State Action -> Action -> StateUpdate Props State Action
 update self Noop = NoUpdate
+update self@{ state } AddItem =
+  Update state { item = "", items = Array.snoc state.items state.item }
+update self@{ state } (EditItem v) =
+  Update state { item = v }
+
